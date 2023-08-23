b0822e8c6ba74456a16e6bbe8e670d5ceea8b8c3
JerryEsperanza <jeremias.esperanza@gmail.com>
2023-08-23 12:15:07 +0800

    updating Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is ESPERANZA, Jeremias!");
+    System.out.println("What a complicated world of SoftDev!");
   }
 }
