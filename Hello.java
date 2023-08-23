a7951d00c1f4c0a4208394ef8fdc6000952ad190
JerryEsperanza <jeremias.esperanza@gmail.com>
2023-08-23 14:41:54 +0800

    added new MyTestFile.txt

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello{
-  public static void main(String[] args){
-    System.out.println("Hello, Software Engineering Course!");
-    System.out.println("My name is ESPERANZA, Jeremias!");
-    System.out.println("What a complicated world of SoftDev!");
-    System.out.println("That is why developers are highly-paid!");
-  }
-}
