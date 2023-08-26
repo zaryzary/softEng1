5061a39c4cb1bba2851aea649a122f2c96e171ea
Eliezer Paredes <zryprds@gmail.com>
2023-08-26 17:59:32 +0800

    added a new file MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Software Engineering Course!");
 		System.out.println("My name is PAREDES, Eliezer!");
+		System.out.println ("Complicated world of SoftDev that is why it pays much!");
 	}
 }
