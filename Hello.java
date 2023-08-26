5a274b7492c134d6e15ab9c88158a69507d18b2e
Eliezer Paredes <zryprds@gmail.com>
2023-08-26 16:16:50 +0800

    added a new file Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello Software Engineering Course!");
+		System.out.println("My name is PAREDES, Eliezer!");
+	}
+}
